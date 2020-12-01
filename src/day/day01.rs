fa71e2eb3a12f414edfad194ec312bab50c77c9e
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-01 12:59:20 +0000

    Add code for executing day 1

---
+++ src/day/day01.rs
@@ -0,0 +1,3 @@
+pub fn day01() {
+    println!("day1");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+
+mod day;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+
+    match args.nth(1) {
+        Some(day) => run_day(day),
+        None => panic!("No day given"),
+    }
+}
+
+fn run_day(day: String) {
+    println!("{}", day);
+
+    if day == "1" {
+        day::day01::day01();
+    }
 }
