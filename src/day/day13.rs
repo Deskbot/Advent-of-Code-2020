9312209689449ab67f308aaa01521fd9dfb12f4d
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-13 16:47:10 +0000

    stuff

--- src/day/day13.rs
+++ src/day/day13.rs
@@ -5,7 +5,7 @@ pub fn day13() {
     let file = fs::read_to_string("input/day13.txt").expect("input not found");
 
     println!("Part 1: {}", part1(&file));
-    println!("Part 2: {}", part2(&file, 100000000000000));
+    println!("Part 2: {}", part2(&file));
 }
 
 fn part1(input: &str) -> i64 {
@@ -34,16 +34,16 @@ fn part1(input: &str) -> i64 {
     return bus_id * wait_time;
 }
 
-fn part2(input: &str, start_at: i64) -> i64 {
+fn part2(input: &str) -> i64 {
     let itr = input.lines();
     let mut itr = itr.skip(1); // skip current time line
 
-    let itr = itr.next().unwrap().split(',');
+    let buses = itr.next().unwrap().split(',').collect::<Vec<&str>>();
 
     // create some kind of data structure for when each bus should arrive
     let mut bus_to_offset = HashMap::<i64,i64>::new();
 
-    for (offset, bus_id_str) in itr.enumerate() {
+    for (offset, &bus_id_str) in buses.iter().enumerate() {
         if bus_id_str == "x" {
             continue;
         }
@@ -55,8 +55,17 @@ fn part2(input: &str, start_at: i64) -> i64 {
 
     // a time is the correct answer if for each bus id: (id - (time % id)) == minute_it_should_arrive
 
+    let start_at = bus_to_offset.keys().fold(1, |acc,next| acc * next);
+    let mult = buses.iter().min().unwrap().parse::<i64>().unwrap(); // the answer is a multiple of this
+
+    let mut minute = start_at;
+
     'outer:
-    for minute in start_at.. {
+    loop {
+        minute -= mult;
+
+        println!("{}", minute);
+
         for (&id, &bus_req_offset) in &bus_to_offset {
             if (minute + bus_req_offset) % id != 0 {
                 continue 'outer; // this minute is not such a minute, try the next minute
@@ -65,8 +74,6 @@ fn part2(input: &str, start_at: i64) -> i64 {
 
         return minute;
     }
-
-    panic!("no such minute found");
 }
 
 
@@ -84,16 +91,16 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(EXAMPLE, 1068700), 1068781);
+        assert_eq!(part2(EXAMPLE), 1068781);
     }
 
     #[test]
     fn part2_example2() {
-        assert_eq!(part2("poop\n7,13", 0), 77);
+        assert_eq!(part2("poop\n7,13"), 77);
     }
 
     #[test]
     fn part2_example3() {
-        assert_eq!(part2("poop\n17,x,13,19", 0), 3417);
+        assert_eq!(part2("poop\n17,x,13,19"), 3417);
     }
 }
