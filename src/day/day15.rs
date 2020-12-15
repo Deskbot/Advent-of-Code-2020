3951f6a1ee5eb471cf29376cbabc745e1c8841c5
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-15 12:40:48 +0000

    track time number last appeared in starting numbers

--- src/day/day15.rs
+++ src/day/day15.rs
@@ -1,12 +1,17 @@
+use std::collections::HashMap;
+
 
 pub fn day15() {
-    println!("Part 1: {}", part1("15,5,1,4,7,0"));
+    println!("Part 1: {}", part1(&[15,5,1,4,7,0]));
     // println!("Part 2: {}", part2(&file));
 }
 
-fn part1(input: &str) -> i64 {
+fn part1(starting_numbers: &[i64]) -> i64 {
+    let mut last_position = HashMap::<usize,i64>::new();
 
-
+    for (index, &num) in starting_numbers.iter().enumerate() {
+        last_position.insert(index, num);
+    }
 
     0
 }
