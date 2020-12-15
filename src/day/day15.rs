cef9da0399813a9716fdcff367ee09aef0e15a17
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-15 15:23:42 +0000

    Insert into the map at the right time
    oh boy, I can't wait to have this implementation btfoed by part 2

--- src/day/day15.rs
+++ src/day/day15.rs
@@ -7,10 +7,10 @@ pub fn day15() {
 }
 
 fn part1(starting_numbers: &[i64]) -> i64 {
-    let mut last_position = HashMap::<i64,i64>::new();
+    let mut penultimate_turns = HashMap::<i64,i64>::new();
 
     for (index, &num) in starting_numbers.iter().enumerate() {
-        last_position.insert(num, 1 + index as i64); // 1 indexed
+        penultimate_turns.insert(num, 1 + index as i64); // 1 indexed
     }
 
     let mut last_number = *starting_numbers.last().unwrap();
@@ -19,19 +19,18 @@ fn part1(starting_numbers: &[i64]) -> i64 {
     while last_turn < 2020 {
         let this_number;
 
-        if let Some(&index_last_time) = last_position.get(&last_number) {
-            this_number = last_turn - index_last_time;
+        if let Some(&penultimate_turn) = penultimate_turns.get(&last_number) {
+            this_number = last_turn - penultimate_turn;
         } else {
             this_number = 0;
         }
 
-        let this_turn = last_turn + 1;
-        last_position.insert(this_number, this_turn);
+        penultimate_turns.insert(last_number, last_turn);
 
         last_turn += 1;
         last_number = this_number;
 
-        println!("{:?}", last_position);
+        println!("{:?}", penultimate_turns);
     }
 
     assert_eq!(last_turn, 2020);
