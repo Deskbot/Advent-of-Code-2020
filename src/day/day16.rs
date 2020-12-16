0efc844468ec872bf448c1ff4125f5cbf60e9f8f
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-16 18:23:28 +0000

    stab at part 1

--- src/day/day16.rs
+++ src/day/day16.rs
@@ -1,11 +1,19 @@
 use regex::Regex;
 use std::fs;
 
+#[derive(Clone)]
 struct Range {
     min: i64,
     max: i64,
 }
 
+impl Range {
+    pub fn within(&self, n: i64) -> bool {
+        self.min <= n && n <= self.max
+    }
+}
+
+#[derive(Clone)]
 struct Rule {
     name: String,
     // luckily it looks like these ranges don't overlap
@@ -14,7 +22,6 @@ struct Rule {
 }
 
 impl Rule {
-
     pub fn parse(s: &str) -> Rule {
         let reg = Regex::new(r"([a-z ]+): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)").unwrap();
         let itr = reg.captures(s).unwrap();
@@ -35,6 +42,28 @@ impl Rule {
             range_2,
         }
     }
+
+    pub fn within(&self, n: i64) -> bool {
+        self.range_1.within(n) && self.range_2.within(n)
+    }
+}
+
+struct Ticket {
+    numbers: Vec<i64>
+}
+
+impl Ticket {
+    pub fn parse(s: &str) -> Ticket {
+        let numbers = s.split(',')
+            .filter(|&chunk| chunk != "") // this is needed for some reason
+            .map(str::parse::<i64>)
+            .map(Result::unwrap)
+            .collect::<Vec<i64>>();
+
+        return Ticket {
+            numbers
+        }
+    }
 }
 
 pub fn day16() {
@@ -50,10 +79,31 @@ fn part1(input: &str) -> i64 {
     let my_ticket = chunks.next().unwrap();
     let nearby_tickets = chunks.next().unwrap();
 
+    // parse stuff
+
     let rules = rules
         .split("\n")
-        .map(Rule::parse);
+        .map(Rule::parse)
+        .collect::<Vec<Rule>>();
 
+    let my_ticket = my_ticket.split("\n").last().unwrap();
+    let my_ticket = Ticket::parse(my_ticket);
 
-    0
+    let nearby_tickets = nearby_tickets
+        .split("\n").skip(1)
+        .map(Ticket::parse);
+
+    let mut error_rate = 0;
+
+    for ticket in nearby_tickets {
+        for number in ticket.numbers {
+            let invalid = rules.iter().any(|rule| !rule.within(number));
+            if invalid {
+                error_rate += number;
+                break;
+            }
+        }
+    }
+
+    return error_rate;
 }
