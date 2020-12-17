72b32f792740a29dc69c047f3af807ed91aad850
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-17 12:40:20 +0000

    Most of part 1

--- src/day/day17.rs
+++ src/day/day17.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 use std::fs;
+use itertools::Itertools;
 
-#[derive(Clone)]
+#[derive(Clone, Eq, PartialEq)]
 enum State {
     Alive,
     Dead,
@@ -15,6 +16,23 @@ impl State {
             _ => panic!("invalid char"),
         }
     }
+
+    pub fn next(&self, neighbours: i64) -> State {
+        let is_alive = match self {
+            State::Alive => {
+                neighbours == 2 || neighbours == 3
+            }
+            State::Dead => {
+                neighbours == 3
+            }
+        };
+
+        if is_alive {
+            State::Alive
+        } else {
+            State::Dead
+        }
+    }
 }
 
 struct Conway3D {
@@ -26,6 +44,22 @@ impl Conway3D {
         Conway3D {
             space: HashMap::new()
         }
+    }
+
+    pub fn alive_count(&self) -> i64 {
+        let mut alive_count = 0;
+
+        for plane in self.space.values() {
+            for row in plane.values() {
+                for &cell in row.values() {
+                    if cell == State::Alive {
+                        alive_count += 1;
+                    }
+                }
+            }
+        }
+
+        return alive_count;
     }
 
     fn get(&mut self, x: i64, y: i64, z: i64) -> &State {
@@ -50,6 +84,40 @@ impl Conway3D {
                 self.insert(cell.clone(), x as i64, y as i64, 0)
             }
         }
+    }
+
+    fn neighbours(&self, x: i64, y: i64, z: i64) -> Vec<&State> {
+        // I thought writing all the combinations would make it too easy to miss one.
+
+        let numbers = vec![-1,0,1];
+        let offsets = numbers.iter().combinations(3)
+            .map(|list| list.iter())
+            .map(|list| (list.next().unwrap(), list.next().unwrap(), list.next().unwrap()))
+            .filter(|(&&a,&&b,&&c)| !(a==0 && b==0 && c==0));
+
+        return offsets
+            .map(|(&&x,&&y,&&z)| self.get(x,y,z))
+            .collect();
+    }
+
+    pub fn step(&mut self) -> Conway3D {
+        let c = Conway3D::new();
+
+        for (&x, plane) in &self.space {
+            for (&y, row) in plane {
+                for (&z, cell) in row {
+                    let neighbours = self.neighbours(x,y,z);
+                    let alive_neighbours = neighbours.into_iter()
+                        .filter(|&&n| n == State::Alive)
+                        .count() as i64;
+                    let current_state = self.get(x, y, z);
+                    let next_state = current_state.next(alive_neighbours);
+                    c.insert(next_state, x, y, z);
+                }
+            }
+        }
+
+        return c;
     }
 }
 
@@ -77,8 +145,10 @@ fn part1(input: &str) -> i64 {
     conway.insert_plane(input_plane);
 
     // increment 6 times
+    for _ in 1..=6 {
+        conway.step();
+    }
 
     // return count alive
-
-    0
+    return conway.alive_count();
 }
