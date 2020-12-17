ae8af36ccf920e33dcd4ec94c4d7005e30fb6483
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-17 11:20:10 +0000

    Parse input

--- src/day/day17.rs
+++ src/day/day17.rs
@@ -1,5 +1,20 @@
 use std::fs;
 
+enum State {
+    Alive,
+    Dead,
+}
+
+impl State {
+    pub fn parse(c: &char) -> State {
+        match c {
+            '#' => State::Alive,
+            '.' => State::Dead,
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
 pub fn day17() {
     let file = fs::read_to_string("input/day16.txt").expect("input not found");
 
@@ -8,5 +23,22 @@ pub fn day17() {
 }
 
 fn part1(input: &str) -> i64 {
+    // make conway structure
+
+    // parse input
+    let input_plane = input.split("\n\n")
+        .map(
+            |line| line.chars()
+                .map(|c| State::parse(&c))
+                .collect()
+        )
+        .collect::<Vec<Vec<State>>>();
+
+    // insert plane
+
+    // increment 6 times
+
+    // return count alive
+
     0
 }
