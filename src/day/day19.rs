86b99572957031b047947448976a51a85596645f
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-19 10:35:39 +0000

    Set up day 19

---
+++ src/day/day19.rs
@@ -0,0 +1,13 @@
+use std::str::Chars;
+use std::fs;
+
+pub fn day19() {
+    let file = fs::read_to_string("input/day19.txt").expect("input not found");
+
+    println!("Part 1: {}", part1(&file));
+    // println!("Part 2: {}", part2(&file));
+}
+
+fn part1(input: &str) -> i64 {
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,7 @@ fn run_day(day: usize) {
         day::day16::day16,
         day::day17::day17,
         day::day18::day18,
+        day::day19::day19,
     ];
 
     let day_index = day - 1;
