9d2513b548a14ba54682cab7a678a773aa9ec63a
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-19 18:52:21 +0000

    stuff

--- src/day/day19.rs
+++ src/day/day19.rs
@@ -5,7 +5,7 @@ use std::fs;
 pub fn day19() {
     let file = fs::read_to_string("input/day19.txt").expect("input not found");
 
-    println!("Part 1: {}", part1(&file));
+    // println!("Part 1: {}", part1(&file));
     println!("Part 2: {}", part2(&file));
 }
 
@@ -96,13 +96,28 @@ fn part2(input: &str) -> i64 {
     rules_map.insert(11, rule_11);
     rules_map.insert(-11, rule_negative_11);
 
+    // like this we would still have problems due to
+    // in the case of 0: 8 11
+    // we might get
+    // input: aab
+    // if 8 is effectively "a", we would consume both "a"s meaning the rest of the string doesn't match 11,
+    // although it can
+
+    // solution: parse right to left lmao
+
+    let mut backwards_rules_map = HashMap::<i64,Rule>::new();
+    for (key, val) in rules_map {
+        backwards_rules_map.insert(key, val.reverse());
+    }
+
     return messages_str
         .lines()
         .filter(|message| {
-            let mut s = message.chars();
+            let message_reversed = message.chars().rev().collect::<String>();
+            let mut s = message_reversed.chars();
 
-            let rule_passes = rules_map.get(&0).unwrap()
-                .pass(&mut s, &rules_map);
+            let rule_passes = backwards_rules_map.get(&0).unwrap()
+                .pass(&mut s, &backwards_rules_map);
 
             // rule passes and there's no more characters that need checking
             return rule_passes && s.count() == 0;
@@ -110,6 +125,7 @@ fn part2(input: &str) -> i64 {
         .count() as i64;
 }
 
+#[derive(Clone)]
 enum Step {
     SubRule(i64),
     Char(char),
@@ -192,6 +208,7 @@ impl Rule {
     }
 
     pub fn pass(&self, s: &mut Chars, rules: &HashMap<i64,Rule>) -> bool {
+        // println!("{} {}", self.number, s.clone().collect::<String>());
         // return true if any sub rule passes
 
         for seq in &self.sequences {
@@ -206,6 +223,20 @@ impl Rule {
         return false;
     }
 
+    pub fn reverse(&self) -> Rule {
+        let mut reversed_sequences = self.sequences.clone();
+        reversed_sequences.reverse();
+        reversed_sequences
+            .iter_mut()
+            .for_each(|seq| {
+                seq.reverse();
+            });
+
+        Rule {
+            number: self.number,
+            sequences: reversed_sequences,
+        }
+    }
 }
 
 
@@ -253,4 +284,107 @@ b
 aa
 "), 0);
     }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2("42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: \"a\"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: \"b\"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+"), 12);
+    }
+
+    #[test]
+    fn part2_example_smaller() {
+        assert_eq!(part2("42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: \"a\"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: \"b\"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+bbabbbbaabaabba
+"), 12);
+    }
+
+//     #[test]
+//     fn part2_test() {
+//         assert_eq!(part2("0: 8 11
+// 8: 2
+// 11: 3 4
+// 2: \"a\"
+// 3: \"a\"
+// 4: \"b\"
+
+// aab
+// "), 1);
+//     }
 }
