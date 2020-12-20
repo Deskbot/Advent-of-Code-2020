406e7bc67c8a3b6d77ae97df5487893d295d413a
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-20 11:21:34 +0000

    Add outline

--- src/day/day20.rs
+++ src/day/day20.rs
@@ -10,5 +10,16 @@ pub fn day20() {
 }
 
 fn part1(input: &str) -> i64 {
+    // split into tile strings
+
+    // parse tiles
+    // into struct with tile num and 2d grid
+    // add tile method for getting each edge
+
+    // for each tile (my_tile)
+    // for each other tile
+    // count how many of (my_tile)'s edges are unique
+
+    // *= tiles that have exactly 2 unique edges
     0
 }
