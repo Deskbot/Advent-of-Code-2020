960cd5fd3875aa4faa1738b6803d59b8e8999ed0
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-21 13:38:26 +0000

    Parse input

--- src/day/day21.rs
+++ src/day/day21.rs
@@ -10,5 +10,34 @@ pub fn day21() {
 }
 
 fn part1(input: &str) -> i64 {
+    input.lines()
+        .map(Food::parse)
+        .for_each(|a| println!("{:?}", a));
     0
+}
+
+#[derive(Debug)]
+struct Food<'a> {
+    allergens: Vec<&'a str>,
+    ingredients: Vec<&'a str>,
+}
+
+impl<'a> Food<'a> {
+    pub fn parse(s: &str) -> Food {
+        let mut itr = s.split(" (contains ");
+        let ingredients_str = itr.next().unwrap();
+        let allergens_str = itr.next().unwrap();
+
+        let allergens = allergens_str.split(")").next().unwrap()
+            .split(" ")
+            .collect();
+        let ingredients = ingredients_str
+            .split(" ")
+            .collect();
+
+        return Food {
+            allergens,
+            ingredients,
+        };
+    }
 }
